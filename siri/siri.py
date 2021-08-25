8a4ec69169085a4bca5aac322c890c7c9030dc7c
Xstro <87899752+Xstro@users.noreply.github.com>
2021-08-25 09:04:49 +0530

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -67,7 +67,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[ᴍᴀʟʟᴜ.ᴍᴜsɪᴄ]" 
+            performer = f"[ᴀʟᴀɴ.ᴡᴀʟᴋᴇʀ]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
@@ -82,13 +82,13 @@ def a(client, message):
         )
         print(str(e))
         return
-    m.edit("`sᴛɪʟʟ... ᴜᴘʟᴏᴀᴅɪɴɢ... ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...`")
+    m.edit("`sᴛɪʟʟ..ᴜᴘʟᴏᴀᴅɪɴɢ..ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ..`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎶 <b>ᴛɪᴛʟᴇ:</b> <a href="{link}">{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n📻 <b>ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ:</b> <a href="https://t.me/musicupdatez">ᴍᴀʟʟᴜ.ᴍᴜsɪᴄ</a>'
+        rep = f'🎶 <b>ᴛɪᴛʟᴇ:</b> <a href="{link}">{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n📻 <b>ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ:</b> <a href="https://t.me/musicupdatez">ᴍᴜsɪᴄ.ᴜᴘᴅᴀᴛᴇs</a>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
